9f2df64284c215ce6d54f0ff99ee4de48379a82f
Yasar Kücükkaya <yasar@yk-code.de>
2018-04-26 22:59:31 +0200

    fix(chore): Update to support nuxt 1.4

--- test/module.test.js
+++ test/module.test.js
@@ -3,22 +3,17 @@ process.env.PORT = process.env.PORT || 5060
 process.env.NODE_ENV = 'production'
 
 const { Nuxt, Builder } = require('nuxt')
-const request = require('request-promise-native')
+const axios = require('axios')
 
 const config = require('./fixture/nuxt.config')
 
 const url = path => `http://localhost:${process.env.PORT}${path}`
-const get = path => request(url(path))
+const get = path => axios.get(url(path))
 
 describe('Module', () => {
   let nuxt
-
+  //
   beforeAll(async () => {
-    config.modules.unshift(function () {
-      // Add test specific test only hooks on nuxt life cycle
-    })
-
-    // Build a fresh nuxt
     nuxt = new Nuxt(config)
     await new Builder(nuxt).build()
     await nuxt.listen(process.env.PORT)
@@ -30,7 +25,7 @@ describe('Module', () => {
   })
 
   test('render', async () => {
-    let html = await get('/')
-    expect(html).toContain('Markup:')
+    let response = await get('/')
+    expect(response.data).toContain('Markup:')
   })
 })
