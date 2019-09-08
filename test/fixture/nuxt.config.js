b32bc8e5957396280915b1e3b3f7d3ab3835e261
Yasar Kücükkaya <yasar@yk-code.de>
2019-09-09 00:40:31 +0200

    Cleanup, fixed tests and added prettier.

--- test/fixture/nuxt.config.js
+++ test/fixture/nuxt.config.js
@@ -1,16 +1,10 @@
-const { resolve } = require('path')
-
 module.exports = {
-  rootDir: resolve(__dirname, '../../'),
-  buildDir: resolve(__dirname, '.nuxt'),
   srcDir: __dirname,
   dev: false,
   render: {
     resourceHints: false
   },
-  modules: [{
-    handler: require('../../')
-  }],
+  modules: [require('../../')],
   wp: {
     endpoint: 'https://wp.kmr.io/wp-json'
   }
